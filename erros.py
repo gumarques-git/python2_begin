f4733fccdb5ab0e63e3655f388ed013a61f9fa95
Gustavo <gumarques@gmail.com>
2019-08-17 02:26:49 +0100

    Try without Except

--- erros.py
+++ erros.py
@@ -9,8 +9,6 @@ try:
     arquivo = open(nome_arquivo, 'r')
     valores = arquivo.readline().split(',') #usamos dois ponto ':' para simular erro
     perfil = Perfil(*valores)
-except (IOError, TypeError) as erro:
-    print('Deu Exception: %s' % erro)
 finally:
     if(arquivo is not None):
         print('Fechando o arquivo: %s' % nome_arquivo)
